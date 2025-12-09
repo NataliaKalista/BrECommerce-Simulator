55a16dd128aa1695e68b829e60257732f790ba58
Natalka K <natalia.kalista@wp.pl>
2025-12-09 12:42:53 +0100

    Updated dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,10 +12,11 @@ if not os.path.exists('brEcommerce.db'):
 st.title("E-Commerce Dashboard")
 
 
-page = st.sidebar.selectbox('Choose a view', ['Category Rank','Orders Map','Sales Trends','Business Simulator'])
+page = st.sidebar.selectbox('Choose a view', ['Category Rank','Orders Map','Sales Trends','Business Simulator','Orders Status','Payment Types','Customer Satisfaction','Peak Hours'])
 
 if page== 'Category Rank':
     st.header("Top 5 Categories in Brazil")
+    st.divider()
 
     def get_data():
         conn = sqlite3.connect('brEcommerce.db')
@@ -60,6 +61,7 @@ if page== 'Category Rank':
 
 elif page == 'Orders Map':
     st.header("Geolocation of Customers")
+    st.divider()
     
     def get_map_data():
         conn2 = sqlite3.connect('brEcommerce.db')
@@ -82,6 +84,7 @@ elif page == 'Orders Map':
 
 elif page == 'Sales Trends':
     st.header('Sales Trends 2016-2018')
+    st.divider()
 
     def get_sales_data():
         conn3 = sqlite3.connect('brEcommerce.db')
@@ -119,6 +122,7 @@ elif page == 'Sales Trends':
 
 elif page == 'Business Simulator':
     st.header('Business Simulator')
+    st.divider()
 
     def get_price_freight():
         conn4 = sqlite3.connect('brEcommerce.db')
@@ -126,7 +130,6 @@ elif page == 'Business Simulator':
         SELECT SUM(oi.price) AS current_revenue, SUM(oi.freight_value) AS current_freight
         FROM orders o JOIN order_items oi ON o.order_id = oi.order_id
         WHERE o.order_status = 'delivered'
-        LIMIT 1000
         """
 
         df_result4 = pd.read_sql(query4, conn4)
@@ -183,7 +186,174 @@ elif page == 'Business Simulator':
                 delta = f"{diff_total:,.0f} BRL"
             )
 
+elif page == 'Orders Status':
+    st.header('Orders Status')
+    st.divider()
+
+    def get_order_stat():
+        conn5 = sqlite3.connect('brEcommerce.db')
+        query5 = """
+        SELECT o.order_status, COUNT(*) AS Orders
+        FROM orders o 
+        GROUP BY order_status
+        """
+
+        df_result5 = pd.read_sql(query5, conn5)
+        conn5.close()
+        return df_result5
+
+    df_status = get_order_stat()
+
+    try:
+        delivered_val = df_status[df_status['order_status'] == 'delivered']['Orders'].values[0]
+    except IndexError:
+        delivered_val = 0
+
+    df_others = df_status[df_status['order_status'] != 'delivered']
+
+    col1, col2 = st.columns([2,1])
+
+    with col1:
+        st.subheader("Visualization")
+
+        fig = px.pie(
+            df_others,
+            values='Orders',
+            names='order_status',
+            title="Status proportions (excluding delivered)"
+        )
+
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        fig.update_layout(showlegend=False)
+        st.plotly_chart(fig, use_container_width=True) 
+
+    with col2:
+        st.subheader("Details")   
+
+        st.metric(label="Delivered", value=f"{delivered_val:,}")
+        st.divider()
+
+        st.dataframe(df_status, hide_index=True, use_container_width = True)
+
+elif page == 'Payment Types':
+    st.header('Payment Types')
+    st.divider()
+
+    def get_type():
+        conn6 = sqlite3.connect('brEcommerce.db')
+        query6 = """
+        SELECT payment_type, COUNT(*) AS payments
+        FROM order_payments
+        WHERE payment_type != 'not_defined'
+        GROUP BY payment_type
+        """
+
+        df_result6 = pd.read_sql(query6, conn6)
+        conn6.close()
+        return df_result6
+    
+    df_type = get_type()
+
+    col1, col2 = st.columns([2,1])
+
+    with col1:
+        st.subheader('Visualization')
+        fig = px.pie(
+                df_type,
+                values='payments',
+                names='payment_type',
+                title="Payment proportions"
+            )
+
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        fig.update_layout(showlegend=False)
+        st.plotly_chart(fig, use_container_width=True) 
+
+    with col2:
+        st.subheader('Details')
+        st.dataframe(df_type, hide_index=True, use_container_width = True)
 
 
+elif page == 'Customer Satisfaction':
+    st.header("Customer Satisfaction")
+    st.divider()
+
+    def get_reviews():
+        conn7 = sqlite3.connect('brEcommerce.db')
+        query7 = """
+        SELECT review_score, COUNT(*) AS number_of_scores
+        FROM order_reviews
+        GROUP BY review_score
+        ORDER BY review_score DESC
+        """
+
+        df_result7 = pd.read_sql(query7, conn7)
+        conn7.close()
+        return df_result7
+    
+    df_reviews = get_reviews()
+
+    col1, col2 = st.columns([2,1])
+
+    with col1:
+        st.subheader('Visualization')
+
+        fig = px.bar(
+        df_reviews, 
+        x='review_score', 
+        y='number_of_scores',
+        color_discrete_sequence=["#a06ae7"], 
+        text_auto='.2s' 
+        )
+        
+        fig.update_layout(
+            xaxis_title="Scores",
+            yaxis_title="Sum",
+            plot_bgcolor="rgba(0,0,0,0)", 
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with col2:
+        st.subheader('Details')
+
+        st.dataframe(df_reviews,hide_index=True, use_container_width=True)
+
+elif page == 'Peak Hours':
+    st.header('Peak Hours')
+    st.divider()
+
+    def get_hours():
+        conn8 = sqlite3.connect('brEcommerce.db')
+        query8 = """
+        SELECT strftime('%H', order_purchase_timestamp) AS Hour,
+                Count (*) AS Orders
+        FROM orders
+        GROUP BY Hour
+        ORDER BY Hour
+        """
+
+        df_result8 = pd.read_sql(query8, conn8)
+        conn8.close()
+        return df_result8
+    
+    df_hours = get_hours()
+    max_index = df_hours['Orders'].idxmax()
+    top_hour = df_hours.loc[max_index, 'Hour']
+
+    st.info(f"Peak hour is **{top_hour}:00**")
+    st.subheader('Customers Activity during the Day')
+
+    fig = px.bar(
+        df_hours,
+        x='Hour',
+        y='Orders',
+        labels={'Hour': '00-23', 'Orders':'Counted Orders'}
+    )
+
+    fig.update_traces(marker_color = df_hours['Orders'], marker_colorscale='Purples')
+    fig.update_layout(xaxis=dict(tickmode='linear'))
+
+    st.plotly_chart(fig, use_container_width=True)
 
 
